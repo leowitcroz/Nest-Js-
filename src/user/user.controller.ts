a12e6b0f1ef50ff624b314714fc797c85c375afb
leowitcroz <leowitcroz@gmail.com>
2023-11-16 20:39:25 -0300

    readAll e readOne controllers

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
-
   @Post()
   async create(@Body() body) {
-    return {body};
+    return { body };
+  }
+
+  @Get()
+  async readAll() {
+    return { users:[{}]};
+  }
+
+  @Get(':id')
+  async readOne(@Param() params){
+    return { user:{}, params}
   }
 }
+
+// o @Param pega os paremetros da url
